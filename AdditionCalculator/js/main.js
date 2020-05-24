c6a02651fd166471424f9a9a981c210ab5438da8
JMSci <40440123+JMCSci@users.noreply.github.com>
2020-05-24 15:49:47 -0400

    Created Addition Calculator

--- AdditionCalculator/js/main.js
+++ AdditionCalculator/js/main.js
@@ -1,1 +1,24 @@
-﻿// Your code here!
+﻿// addOne: Adds numbes from 1 to 100;
+function addOne() {
+    var x = 0;
+    for (i = 1; i <= 100; i++) {
+        x += i;
+    }
+    alert(x);
+}
+
+
+// addNumbers: Gets numbers from textfield and add them
+function add() {
+    var x = document.getElementById("number1").value;
+    var y = document.getElementById("number2").value;
+    var z = parseFloat(x) + parseFloat(y);
+    alert(z);
+}
+
+// date: Displays date
+function currentDate() {
+    return new Date();
+}
+document.getElementById("todaydate").innerHTML = currentDate();
+
